65e194ad5fb0434f8dcfaa6427a182f75448875c
Menik <menik.damayanti.p@gmail.com>
2018-01-01 10:43:43 +0700

    create routing for controllers

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,12 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resources([
+	'comments' => 'CommentController',
+	'companies' => 'CompanyController',
+	'projects' => 'Projectcontroller',
+	'roles' => 'RoleController',
+	'tasks' => 'TaskController',
+	'user' => 'UserController'
+]);
