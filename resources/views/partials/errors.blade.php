f75836b9d1fb0222df7d6fe6c352cff377dca5eb
Menik <menik.damayanti.p@gmail.com>
2018-01-01 21:56:24 +0700

    fixing flash message

--- resources/views/partials/errors.blade.php
+++ resources/views/partials/errors.blade.php
@@ -1,5 +1,5 @@
 @if (isset($errors) AND count($errors) > 0)
-	<div class="alert alert-danger alert-dismissible fade show">
+	<div class="alert alert-danger alert-dismissible">
   		<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
  		@foreach ($errors->all() as $error)
  			<li><strong>{!! $error !!}</strong></li>
--- resources/views/partials/success.blade.php
+++ resources/views/partials/success.blade.php
@@ -1,5 +1,5 @@
 @if (session()->has('success'))
-	<div class="alert alert-success alert-dismissible fade show">
+	<div class="alert alert-success alert-dismissible">
   		<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
  		<strong>
  			{!! session()->get('success') !!}
